a66a150cbea7d6cbf6b270dd947783970534414f
jkkCoder <jayeshk118@gmail.com>
2024-09-24 00:04:45 +0530

    @Value annotation

--- src/main/java/net/engineeringdigest/journalApp/service/CodingQuestionService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/CodingQuestionService.java
@@ -1,22 +1,30 @@
 package net.engineeringdigest.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.api.response.QuestionsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 
+@Slf4j
 @Component
 public class CodingQuestionService {
-    private static final String API = "https://letscode.adaptable.app/question/filterQuestions?difficulty=&status=&userId=&itemsPerPage=15&page=1";
+    @Value("${questions.api.endpoint}")
+    private String apiEndpoint;
+    private String API = apiEndpoint + "question/filterQuestions?difficulty=&status=&userId=&itemsPerPage=15&page=1";
 
     @Autowired
     private RestTemplate restTemplate;
 
+
     public QuestionsResponse getQuestions() {
+        log.info(apiEndpoint);
         ResponseEntity<QuestionsResponse> res= restTemplate.exchange(API, HttpMethod.GET, null, QuestionsResponse.class);
         QuestionsResponse body = res.getBody();
         return body;
